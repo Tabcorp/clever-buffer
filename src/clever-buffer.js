4530719eadf918d6d9eaf1c4c723511933f4cba4
nguyenchr <nguyenchr@tabcorp.com.au>
2014-07-18 18:41:10 +1000

    Fix problem when loading module with javascript

--- src/clever-buffer.js
+++ src/clever-buffer.js
@@ -1,4 +1,4 @@
-require("coffee-script");
+require("coffee-script/register");
 
 exports.CleverBufferReader = require("./clever-buffer-reader");
 exports.CleverBufferWriter = require("./clever-buffer-writer");
