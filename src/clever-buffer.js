4b78c701242f4adfd562f258fea1171a550498b1
decaffeinate <deancouch@gmail.com>
2019-12-18 09:29:00 +1100

    decaffeinate: Run post-processing cleanups on clever-buffer-common.coffee and 11 other files

--- src/clever-buffer.js
+++ src/clever-buffer.js
@@ -1,2 +1,4 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 exports.CleverBufferReader = require("./clever-buffer-reader");
 exports.CleverBufferWriter = require("./clever-buffer-writer");
